b58126447b6d21729637666db31cb28660627070
mjmendez <majomendez88mena@gmail.com>
2020-06-11 16:15:08 -0600

    name

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 module.exports = {
-  siteTitle: 'GO MENDEZ', // <title>
+  siteTitle: 'mTekh', // <title>
   manifestName: 'Maria Jose Mendez',
-  manifestShortName: 'MJ Mendez', // max 12 characters
+  manifestShortName: 'mTekh', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/icon_GO_Mendez.png',
-  pathPrefix: `/go-mendez/`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/mTekh/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Maria Jose Mendez',
   subHeading: 'Full time Web Developer. Part time Open source contributor  ',
 };
